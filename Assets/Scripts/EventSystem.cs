169dcda8f2834378181775c6b079143398a63e71
tanaynigam <tanaynigam12@gmail.com>
2017-12-15 21:21:45 -0500

    First Commit

---
+++ Assets/Scripts/EventSystem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class EventSystem : MonoBehaviour {
+
+    public static double freq = 1;
+    public static double amp = 1;
+
+    public static bool pause = true;
+
+	// Use this for initialization
+	void Start () {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Play()
+    {
+        pause = false;
+    }
+
+    public void Pause()
+    {
+        pause = true;
+        Sphere.animator.enabled = false;
+    }
+
+    public void Get_Frequency(string Frequency)
+    {
+        try
+        {
+            freq = Convert.ToDouble(Frequency);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Unable to convert '{0}' to a Double.", freq);
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("'{0}' is outside the range of a Double.", freq);
+        }
+    }
+
+    public void Get_Amplitude(string Amplitude)
+    {
+        try
+        {
+            amp = Convert.ToDouble(Amplitude);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Unable to convert '{0}' to a Double.", amp);
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("'{0}' is outside the range of a Double.", amp);
+        }
+    }
+}
---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sphere : MonoBehaviour {
+
+    GameObject sphere;
+    GameObject square;
+    GameObject wave;
+    GameObject animated;
+    public static Animator animator;
+    Vector3 StartPosition = new Vector3(500, 500, -510);
+    Vector3 StartPosition_animated = new Vector3(502.5f, 502.5f, -510f);
+    float sign = 1;
+
+    // Use this for initialization
+    void Start () {
+        sphere = GameObject.Find("Sphere");
+        square = GameObject.Find("Square");
+        wave = GameObject.Find("Wave");
+        animated = GameObject.Find("Animated");
+        animator = gameObject.GetComponent<Animator>();
+        animator.enabled = false;
+        sphere.transform.position = new Vector3(500, 500, -510);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(EventSystem.pause == false)
+        {
+            Check_Toggle();
+        }
+        else
+        {
+            sphere.transform.position = new Vector3(500, 500, -510);
+        }
+	}
+
+    void Check_Toggle()
+    {
+        if(square.GetComponent<Toggle>().isOn == true)
+        {
+            Square_Translate();
+        }
+        else if(wave.GetComponent<Toggle>().isOn == true)
+        {
+            Wave_Translate();
+        }
+        else if(animated.GetComponent<Toggle>().isOn == true)
+        {
+            Animated_Translate();
+        }
+        else
+        {
+            EventSystem.pause = false;
+        }
+    }
+
+    void Square_Translate()
+    {
+        StartCoroutine(Wait((float)(1/(2*EventSystem.freq))));
+    }
+
+    void Wave_Translate()
+    {
+        
+            sphere.transform.position = StartPosition + new Vector3( 0.0f, (float)EventSystem.amp * Mathf.Sin((float)EventSystem.freq * Time.time), 0.0f);
+        
+    }
+
+    void Animated_Translate()
+    {
+        animator.speed = ((float)(1 / (2 * EventSystem.freq)));
+        animator.enabled = true;
+        
+    }
+
+    IEnumerator Wait(float time)
+    {
+        transform.position = StartPosition + new Vector3(0.0f, sign * (float)EventSystem.amp, 0.0f);
+        yield return new WaitForSeconds(time);
+        sign = -1 * sign;
+        StopAllCoroutines();
+
+    }
+}
